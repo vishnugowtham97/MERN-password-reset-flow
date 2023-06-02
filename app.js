0e6b554534f3b98c8bffd1e360769998a1c99333
vishnugowtham97 <vishnugowtham97@gmail.com>
2023-06-02 18:53:14 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+require("./db/conn");
+const router = require("./routes/router");
+const cors = require("cors");
+const cookiParser = require("cookie-parser");
+const port = 8010;
+
+app.get("/", (req, res) => {
+  res.status(201).json("Server Created Successfully!!!Hooray🙌🥳🙌🥳");
+});
+
+app.use(express.json());
+app.use(cookiParser());
+app.use(cors());
+app.use(router);
+
+app.listen(port, () => {
+  console.log(`server start at port ${port}`);
+});
